bdd3f0b973a1a9dad2d26e2d28c1e152d22a7939
DK_lite <kdk5go@gmail.com>
2019-12-10 00:15:33 +0900

    update

--- engine/card/hana.py
+++ engine/card/hana.py
@@ -6,31 +6,65 @@ class CardHana(CardModule):
     def __init__(self):
         pass
 
+    def get_number(self, sms):
+        re_number = re.findall(r"하나+[카드]*[\(0-9\)\*]*", sms)
+        try:
+            return re_number[0]
+        except Exception as e:
+            return None
+
+    def get_holder(self, sms):
+        re_holder = re.findall(r'[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+[*][ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+', sms)
+        try:
+            return re_holder[0]
+        except Exception as e:
+            return None
+
+    def get_date(self, sms):
+        re_date = re.findall(r'[0-9]*/[0-9]*', sms)
+        try:
+            return re_date[0]
+        except Exception as e:
+            return None
+
+    def get_time(self, sms):
+        re_time = re.findall(r'[0-9]*:[0-9]*', sms)
+        try:
+            return re_time[0]
+        except Exception as e:
+            return None
+
+    def get_amount(self, sms):
+        re_money = re.findall(r'[0-9]*,*[0-9]*,*[0-9]*,*[0-9]+원+', sms)
+        try:
+            return ''.join([ i for i in re_money[0] if i.isdigit()])
+        except Exception as e:
+            return None
+
+    def get_use(self, sms):
+        re_money = re.findall(r'[0-9]*,*[0-9]*,*[0-9]*,*[0-9]+원+', sms)
+        try:
+            return ''.join([ i for i in re_money[1] if i.isdigit()])
+        except Exception as e:
+            return None
+            
     def recognize(self, sms):
 
-        re_number = re.findall(r"하나+[카드]*[\(0-9\)\*]*", sms)
-        re_holder = re.findall(r'[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+[*][ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+', sms)
-        re_date = re.findall(r'[0-9]*/[0-9]*', sms)
-        re_time = re.findall(r'[0-9]*:[0-9]*', sms)
-        re_money = re.findall(r'[0-9]*,*[0-9]*,*[0-9]*,*[0-9]*원+', sms)
+        number = self.get_number(sms)
+        holder = self.get_holder(sms)
+        date = self.get_date(sms)
+        time = self.get_time(sms)
+        amount = self.get_amount(sms)
+        use = self.get_use(sms)
 
-        try:
-            number = re_number[0]
-            holder = re_holder[0]
-            date = re_date[0]
-            time = re_time[0]
-            amount = ''.join([ i for i in re_money[0] if i.isdigit()])
-            use = ''.join([ i for i in re_money[1] if i.isdigit()])
+        print(number, holder, date, time, amount, use)
 
-            return CardInformation() \
-            .set_number(number) \
-            .set_holder(holder) \
-            .set_date(date) \
-            .set_time(time) \
-            .set_amount(amount) \
-            .set_use(use) \
-            .get()
-
-        except Exception as e:
-            return CardInformation().get()
+        return CardInformation() \
+        .set_number(number) \
+        .set_holder(holder) \
+        .set_date(date) \
+        .set_time(time) \
+        .set_amount(amount) \
+        .set_use(use) \
+        .get()
 
--- run.py
+++ run.py
@@ -7,6 +7,15 @@ def main():
     engine.add_card(CardSamsung(), loader.get_keys()['SAMSUNG'])
     engine.add_card(CardHana(), loader.get_keys()['HANA'])
 
+
+
+
+    # "[Web발신]\n삼성카드승인4909황*호\n05/29 09:56 원서접수\n25,800원 일시불"
+
+    # result = engine.recognize('15888900', "[Web발신]\n삼성카드승인4909황*호\n05/29 09:56 원서접수\n25,800원 일시불")
+    # print(result)
+
+
    
     results = []
     for data in loader.get_datas():
